81ad184013ba8e44e5687400dbc527cd185fd380
Sobczak <osobc2@uic.edu>
2022-09-06 08:27:43 -0500

    Finished Project

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # WallDrawing_Project1
+Divide the wall equally in half for a top and bottom. 
+On the top left half of a yellow wall draw a blue square with a thin black border.
+On the top right half of the wall draw a pink quadrilateral with a thin black border that points down towards the bottom right corner.
+On the top right half draw a slightly darker pink triangle with a thin black border that overalaps the first quadrilateral and points towards the upper right corner of the wall. 
+On the bottaom half of the wall divide the wall into 4 equal rows. 
+In the first row draw rounded squares with thick black borders alternating between white and black. Draw the squares until you reach the end of the wall. 
+In the second row draw rounded squares with thick black borders with a pattern of white, white, white, black.
+In the third row do the same as in the first row.
+In the fourth row do the same as in the second row. 
---
+++ sketch.js
@@ -0,0 +1,219 @@
+
+//variables 
+
+let x = 1;
+console.log(x);
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    //top left is (0,0) origin point 
+
+
+  }
+  
+  console.log(" first console log ");
+
+  function draw() {
+  background(255,225,77); //changes color rgb 
+
+  fill(0,149,179);
+  strokeWeight(3);
+  square(windowWidth/4, 100, 250);
+  
+
+  fill(230,0,230);
+  strokeWeight(3);
+  quad(700, 100, 700, 150, 800, 200, 1200, 225);
+  
+
+  fill(255,102,255);
+  quad(700, 200, 700, 400, 1360, 20, 1200, 70);
+
+  strokeWeight(10);
+  fill(255);
+  square(0, windowHeight/2, 100, 20);
+
+  fill(0);
+  square(100, windowHeight/2, 120, 20);
+
+  fill(255);
+  square(200, windowHeight/2, 100, 20);
+
+  fill(0);
+  square(300, windowHeight/2, 120, 20);
+
+  fill(255);
+  square(400, windowHeight/2, 100, 20);
+
+  fill(0);
+  square(500, windowHeight/2, 120, 20);
+
+  fill(255);
+  square(600, windowHeight/2, 100, 20);
+
+  fill(0);
+  square(700, windowHeight/2, 120, 20);
+
+  fill(255);
+  square(800, windowHeight/2, 100, 20);
+
+  fill(0);
+  square(900, windowHeight/2, 120, 20);
+
+  fill(255);
+  square(1000, windowHeight/2, 100, 20);
+
+  fill(0);
+  square(1100, windowHeight/2, 120, 20);
+
+  fill(255);
+  square(1200, windowHeight/2, 100, 20);
+
+  fill(0);
+  square(1300, windowHeight/2, 120, 20);
+
+  fill(255);
+  square(1400, windowHeight/2, 100, 20);
+  //top row ^
+
+  fill(255);
+  square(0, 505, 100, 20);
+
+  fill(255);
+  square(100, 505, 100, 20);
+
+  fill(0);
+  square(200, 505, 120, 20);
+
+  fill(255);
+  square(300, 505, 100, 20);
+
+  fill(255);
+  square(400, 505, 100, 20);
+
+  fill(255)
+  square(500, 505, 100, 20);
+
+  fill(0);
+  square(600, 505, 120, 20);
+
+  fill(255);
+  square(700, 505, 100, 20);
+
+  fill(255);
+  square(800, 505, 100, 20);
+
+  fill(255);
+  square(900, 505, 100, 20);
+
+  fill(0);
+  square(1000, 505, 120, 20);
+
+  fill(255);
+  square(1100, 505, 100, 20);
+
+  fill(255);
+  square(1200, 505, 100, 20);
+
+  fill(255);
+  square(1300, 505, 100, 20);
+
+  fill(0);
+  square(1400, 505, 120, 20);
+  //second row ^
+
+  fill(255);
+  square(0, 605, 100, 20);
+
+  fill(0);
+  square(100, 605, 120, 20);
+
+  fill(255);
+  square(200, 605, 100, 20);
+
+  fill(0);
+  square(300, 605, 120, 20);
+
+  fill(255);
+  square(400, 605, 100, 20);
+
+  fill(0);
+  square(500, 605, 120, 20);
+
+  fill(255);
+  square(600, 605, 100, 20);
+
+  fill(0);
+  square(700, 605, 120, 20);
+
+  fill(255);
+  square(800, 605, 100, 20);
+
+  fill(0);
+  square(900, 605, 120, 20);
+
+  fill(255);
+  square(1000, 605, 100, 20);
+
+  fill(0);
+  square(1100, 605, 120, 20);
+
+  fill(255);
+  square(1200, 605, 100, 20);
+
+  fill(0);
+  square(1300, 605, 120, 20);
+
+  fill(255);
+  square(1400, 605, 100, 20);
+  //thrid row ^
+  fill(255);
+  square(0, 705, 100, 20);
+
+  fill(255);
+  square(100, 705, 100, 20);
+
+  fill(0);
+  square(200, 705, 120, 20);
+
+  fill(255);
+  square(300, 705, 100, 20);
+
+  fill(255);
+  square(400, 705, 100, 20);
+
+  fill(255);
+  square(500, 705, 100, 20);
+
+  fill(0);
+  square(600, 705, 120, 20);
+
+  fill(255);
+  square(700, 705, 100, 20);
+
+  fill(255);
+  square(800, 705, 100, 20);
+
+  fill(255);
+  square(900, 705, 100, 20);
+
+  fill(0);
+  square(1000, 705, 120, 20);
+
+  fill(255);
+  square(1100, 705, 100, 20);
+
+  fill(255);
+  square(1200, 705, 100, 20);
+
+  fill(255);
+  square(1300, 705, 100, 20);
+
+  fill(0);
+  square(1400, 705, 120, 20);
+  //last row ^
+
+    strokeWeight(10);
+
+
+  }
